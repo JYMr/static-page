16dd885212b9fc7bfc3ab210d8e7a0752a3acad6
JYMr <30524229+JYMr@users.noreply.github.com>
2023-03-23 10:09:27 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- <Header></Header>
+    <MyMessage></MyMessage>
+    <Project></Project>
+    <Demo></Demo>
+    <Footer></Footer> -->
+    <!-- <JingDong></JingDong> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+// import Header from "./view/header/index.vue";
+// import MyMessage from "./view/myMessage.vue/index.vue"
+// import Footer from './view/footer/index.vue'
+// import Project from './view/preject/index.vue'
+// import Demo from './view/demo/index.vue'
+
+// import JingDong from './components/Demo/jingdong.vue'
+export default {
+
+  name: 'App',
+  components: {
+    // Header,
+    // MyMessage,
+    // Footer,
+    // Project,
+    // Demo,
+    // JingDong
+  }
+}
+</script>
+
+<style></style>
